3c04dea169c75f8cb8c93218c6f99cd0a8300b88
oliverbisbee <oliverbisbee@gmail.com>
2025-11-20 11:16:55 -0500

    finished Task 2.4

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -30,21 +30,14 @@ public class StatementPrinter {
      * @throws RuntimeException if one of the play types is not known
      */
     public String statement() {
-        int totalAmount = 0;
-        int volumeCredits = 0;
+
         final StringBuilder result = new StringBuilder(
                 "Statement for "
                 + invoice.getCustomer()
                 + System.lineSeparator()
         );
+
         for (Performance p : invoice.getPerformances()) {
-
-            int thisAmount = 0;
-            thisAmount = getThisAmount(p);
-
-            // add volume credits
-            volumeCredits += getVolumeCredits(p);
-
             // print line for this order
             result.append(
                     String.format("  %s: %s (%s seats)%n",
@@ -52,11 +45,27 @@ public class StatementPrinter {
                     usd(getThisAmount(p)),
                     p.getAudience())
             );
-            totalAmount += thisAmount;
         }
-        result.append(String.format("Amount owed is %s%n", usd(totalAmount)));
-        result.append(String.format("You earned %s credits%n", volumeCredits));
+
+        result.append(String.format("Amount owed is %s%n", usd(getTotalAmount())));
+        result.append(String.format("You earned %s credits%n", getTotalVolumeCredits()));
         return result.toString();
+    }
+
+    private int getTotalAmount() {
+        int totalAmount = 0;
+        for (Performance p : invoice.getPerformances()) {
+            totalAmount += getThisAmount(p);
+        }
+        return totalAmount;
+    }
+
+    private int getTotalVolumeCredits() {
+        int volumeCredits = 0;
+        for (Performance p : invoice.getPerformances()) {
+            volumeCredits += getVolumeCredits(p);
+        }
+        return volumeCredits;
     }
 
     private String usd(int amountOfMoney) {
